9902d412caaa7cd7a3877e0ca450a69fa0f254d3
Panha <sokunpanha.panha@gmail.com>
2026-01-05 15:19:16 +0700

    Add database configuration

---
+++ src/config/database.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default registerAs(
+  'database',
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: [__dirname + '/../database/entities/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+    synchronize: false, // NEVER use true in production
+    logging: process.env.NODE_ENV === 'development',
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+  }),
+);
